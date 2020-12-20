934520baed3ff46ff812fc2b6da97fe4eacb5d5a
alaamabruk <alaamabrouk34@gmail.com>
2020-12-20 23:34:51 +0200

    Final Commit

--- src/main/java/com/NaggroTask/security/WebSecurityConfig.java
+++ src/main/java/com/NaggroTask/security/WebSecurityConfig.java
@@ -4,8 +4,10 @@ package com.NaggroTask.security;
 import com.NaggroTask.exception.APIAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,8 +18,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
-
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
